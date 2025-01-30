498ee701211e334b236629c2217bd69048cdb970
woowooxxx <71040500+woowooxxx@users.noreply.github.com>
2025-01-31 04:06:32 +0800

    Update webui.py

--- tools/uvr5/webui.py
+++ tools/uvr5/webui.py
@@ -69,7 +69,8 @@ def uvr(model_name, inp_root, save_root_vocal, paths, save_root_ins, agg, format
         else:
             paths = [path.name for path in paths]
         for path in paths:
-            inp_path = os.path.join(inp_root, path)
+            #inp_path = os.path.join(inp_root, path)
+            inp_path = path
             if(os.path.isfile(inp_path)==False):continue
             need_reformat = 1
             done = 0
@@ -135,25 +136,25 @@ with gr.Blocks(title="UVR5 WebUI") as app:
         gr.Markdown(html_center(i18n("伴奏人声分离&去混响&去回声"),'h2'))
         with gr.Group():
                 gr.Markdown(
-                    value=html_left(i18n("人声伴奏分离批量处理， 使用UVR5模型。") + "<br>" + \
-                        i18n("合格的文件夹路径格式举例： E:\\codes\\py39\\vits_vc_gpu\\白鹭霜华测试样例(去文件管理器地址栏拷就行了)。")+ "<br>" + \
-                        i18n("模型分为三类：") + "<br>" + \
-                        i18n("1、保留人声：不带和声的音频选这个，对主人声保留比HP5更好。内置HP2和HP3两个模型，HP3可能轻微漏伴奏但对主人声保留比HP2稍微好一丁点；") + "<br>" + \
-                        i18n("2、仅保留主人声：带和声的音频选这个，对主人声可能有削弱。内置HP5一个模型；") + "<br>" + \
-                        i18n("3、去混响、去延迟模型（by FoxJoy）：") + "<br>  " + \
-                        i18n("(1)MDX-Net(onnx_dereverb):对于双通道混响是最好的选择，不能去除单通道混响；") + "<br>&emsp;" + \
-                        i18n("(234)DeEcho:去除延迟效果。Aggressive比Normal去除得更彻底，DeReverb额外去除混响，可去除单声道混响，但是对高频重的板式混响去不干净。") + "<br>" + \
-                        i18n("去混响/去延迟，附：") + "<br>" + \
-                        i18n("1、DeEcho-DeReverb模型的耗时是另外2个DeEcho模型的接近2倍；") + "<br>" + \
-                        i18n("2、MDX-Net-Dereverb模型挺慢的；") + "<br>" + \
-                        i18n("3、个人推荐的最干净的配置是先MDX-Net再DeEcho-Aggressive。"),'h4')
+                    value=html_left(#i18n("人声伴奏分离批量处理， 使用UVR5模型。") + "<br>" + \
+                        #i18n("合格的文件夹路径格式举例： E:\\codes\\py39\\vits_vc_gpu\\白鹭霜华测试样例(去文件管理器地址栏拷就行了)。")+ "<br>" + \
+                        #i18n("模型分为三类：") + "<br>" + \
+                        #i18n("1、保留人声：不带和声的音频选这个，对主人声保留比HP5更好。内置HP2和HP3两个模型，HP3可能轻微漏伴奏但对主人声保留比HP2稍微好一丁点；") + "<br>" + \
+                        #i18n("2、仅保留主人声：带和声的音频选这个，对主人声可能有削弱。内置HP5一个模型；") + "<br>" + \
+                        #i18n("3、去混响、去延迟模型（by FoxJoy）：") + "<br>  " + \
+                        #i18n("(1)MDX-Net(onnx_dereverb):对于双通道混响是最好的选择，不能去除单通道混响；") + "<br>&emsp;" + \
+                        #i18n("(234)DeEcho:去除延迟效果。Aggressive比Normal去除得更彻底，DeReverb额外去除混响，可去除单声道混响，但是对高频重的板式混响去不干净。") + "<br>" + \
+                        #i18n("去混响/去延迟，附：") + "<br>" + \
+                        #i18n("1、DeEcho-DeReverb模型的耗时是另外2个DeEcho模型的接近2倍；") + "<br>" + \
+                        #i18n("2、MDX-Net-Dereverb模型挺慢的；") + "<br>" + \
+                        #i18n("3、个人推荐的最干净的配置是先MDX-Net再DeEcho-Aggressive。"),'h4')
                 )
                 with gr.Row():
                     with gr.Column():
                         model_choose = gr.Dropdown(label=i18n("模型"), choices=uvr5_names)
                         dir_wav_input = gr.Textbox(
                             label=i18n("输入待处理音频文件夹路径"),
-                            placeholder="C:\\Users\\Desktop\\todo-songs",
+                            placeholder="vol",
                         )
                         wav_inputs = gr.File(
                             file_count="multiple", label=i18n("也可批量输入音频文件, 二选一, 优先读文件夹")
@@ -172,7 +173,7 @@ with gr.Blocks(title="UVR5 WebUI") as app:
                             label=i18n("指定输出主人声文件夹"), value="output/uvr5_opt"
                         )
                         opt_ins_root = gr.Textbox(
-                            label=i18n("指定输出非主人声文件夹"), value="output/uvr5_opt"
+                            label=i18n("指定输出非主人声文件夹"), value="output/int_opt"
                         )
                         format0 = gr.Radio(
                             label=i18n("导出文件格式"),
